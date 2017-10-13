175f974017a0a98f2d3ca786c9fc93603b01bfcc
frostar <frostar47@gmail.com>
2017-10-14 06:21:50 +0900

    elementを入れてフォームのひな型作った Validationはまだ

---
+++ src/components/Register.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <el-row :gutter="20">
+    <el-col :span="12" :offset="6">
+      <el-form ref="form" :model="form" label-width="120px">
+        <el-form-item label="Name">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="BirthDay">
+          <el-date-picker type="date" v-model="form.birthDay" style="width: 100%;"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="Password">
+          <el-input type="password" v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item label="PasswordConfirm">
+          <el-input type="password" v-model="form.confpass"></el-input>
+        </el-form-item>
+        <el-form-item label="Description">
+          <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">Create</el-button>
+          <el-button>Cancel</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        birthDay: '',
+        password: '',
+        confpass: '',
+        desc: '',
+      },
+    };
+  },
+};
+</script>
+
+<style lang="css">
+</style>
