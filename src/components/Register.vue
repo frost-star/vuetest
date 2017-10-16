f3bd4df000bfc11f529e61a96e0b51eb534d1218
frostar <frostar47@gmail.com>
2017-10-17 06:31:47 +0900

    一通りのValidation完了

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,24 +1,24 @@
 <template lang="html">
   <el-row :gutter="20">
     <el-col :span="12" :offset="6">
-      <el-form ref="form" :model="form" label-width="120px">
-        <el-form-item label="Name">
+      <el-form ref="sform" :model="form" label-width="120px" :rules="rules">
+        <el-form-item label="Name" prop="name">
           <el-input v-model="form.name"></el-input>
         </el-form-item>
-        <el-form-item label="BirthDay">
-          <el-date-picker type="date" v-model="form.birthDay" style="width: 100%;"></el-date-picker>
+        <el-form-item label="Age" prop="age">
+          <el-input v-model.number="form.age"></el-input>
         </el-form-item>
-        <el-form-item label="Password">
+        <el-form-item label="Password" prop="password">
           <el-input type="password" v-model="form.password"></el-input>
         </el-form-item>
-        <el-form-item label="PasswordConfirm">
-          <el-input type="password" v-model="form.confpass"></el-input>
+        <el-form-item label="PasswordConfirm" prop="passconf">
+          <el-input type="password" v-model="form.passconf"></el-input>
         </el-form-item>
-        <el-form-item label="Description">
+        <el-form-item label="Description" prop="desc">
           <el-input type="textarea" v-model="form.desc"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">Create</el-button>
+          <el-button type="primary">Sign Up</el-button>
           <el-button>Cancel</el-button>
         </el-form-item>
       </el-form>
@@ -32,10 +32,40 @@ export default {
     return {
       form: {
         name: '',
-        birthDay: '',
+        age: '',
         password: '',
-        confpass: '',
+        passconf: '',
         desc: '',
+      },
+      rules: {
+        name: [
+          { required: true, message: '入力必須項目です' },
+          { min: 3, max: 12, message: '3～12文字で入力してください' },
+        ],
+        age: [
+          { required: true, message: '入力必須項目です' },
+          { type: 'number', message: '数字で入力してください' },
+          { type: 'number', min: 1, max: 100, message: '1～100の範囲で入力してください' },
+        ],
+        password: [
+          { required: true, message: '入力必須項目です' },
+          { min: 8, message: '8文字以上で入力してください' },
+        ],
+        passconf: [
+          { required: true, message: '入力必須項目です' },
+          {
+            validator: (rule, value, callback) => {
+              if (value !== this.form.password) {
+                return callback(new Error('パスワードが一致しません'));
+              }
+              return callback();
+            },
+            trigger: 'change',
+          },
+        ],
+        desc: [
+          { max: 100, message: '100文字以内で入力してください' },
+        ],
       },
     };
   },
