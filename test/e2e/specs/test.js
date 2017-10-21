20efc18eb63bf2fca12aba2ad8a693e5cfdb6a90
frostar <frostar47@gmail.com>
2017-10-22 06:25:28 +0900

    テスト1ケース作成

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,18 +2,34 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function test(browser) {
+  //'default e2e tests': function test(browser) {
     // automatically uses dev Server port from /config.index.js
     // default: http://localhost:8080
     // see nightwatch.conf.js
+  //   const devServer = browser.globals.devServerURL;
+  //
+  //   browser
+  //     .url(devServer)
+  //     .waitForElementVisible('#app', 5000)
+  //     .assert.elementPresent('.hello')
+  //     .assert.containsText('h1', 'Welcome to Your Vue.js App')
+  //     .assert.elementCount('img', 1)
+  //     .end();
+  // },
+  'case 1': function test(browser) {
     const devServer = browser.globals.devServerURL;
-
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .setValue('#name input', 'abcdefgh')
+      .setValue('#age input', 100)
+      .setValue('#password input', 'abcdefgh')
+      .setValue('#passconf input', 'abcdefgh')
+      .setValue('#desc textarea', 'abcdefghij')
+      .click('#btn-signup')
+      .assert.containsText('#result span', 'success')
+      .saveScreenshot('./ss/' + browser.currentTest.name + '.png')
       .end();
   },
+
 };
